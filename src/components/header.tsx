f3e02c613a8ba8e833db78a040bb073c9940380e
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-07-30 07:39:45 +0200

    feat: Add header component for navigation and user authentication

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { useSession } from 'next-auth/react';
+import { Button } from '@/components/ui/button';
+
+import Link from 'next/link';
+import { Camera } from 'lucide-react';
+import { UserDropdown } from './user-dropdown';
+
+export function Header() {
+	const { data: session, status } = useSession();
+
+	return (
+		<header className="w-full flex justify-between p-5 border-b lg:relative fixed bg-background z-50">
+			<div className="flex justify-start items-center">
+				<Link href="/" className="flex gap-2 justify-center items-center">
+					<Camera className="w-8 h-8" />
+					<h1 className="text-2xl font-black">Pictures</h1>
+				</Link>
+			</div>
+			{status === 'unauthenticated' ? (
+				<Button asChild>
+					<Link href="/auth/signin">Login</Link>
+				</Button>
+			) : (
+				<UserDropdown />
+			)}
+		</header>
+	);
+}
