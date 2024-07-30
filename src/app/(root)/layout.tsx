2e62b28462f016a76db75e83694f90abb5c232d5
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-07-30 07:38:49 +0200

    feat: Add UI components for skeleton, layout, label, separator, toaster, input, and badge

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+import React from 'react';
+
+import { MobileNav } from '@/components/mobile/navbar';
+import { Header } from '@/components/header';
+
+export default function RootLayout({ children, session }: { children: ReactNode; session?: Session }) {
+	return (
+		<SessionProvider session={session}>
+			<Header />
+
+			<MobileNav />
+
+			<main className="">{children}</main>
+		</SessionProvider>
+	);
+}
