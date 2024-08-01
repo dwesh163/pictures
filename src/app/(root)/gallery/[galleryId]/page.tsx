22169f79f279e4bc5dc02e7a62c6bb9e4c5816dc
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-01 16:00:06 +0200

    feat: Add gallery edit page and view page components

---
+++ src/app/(root)/gallery/[galleryId]/page.tsx
@@ -0,0 +1,50 @@
+'use server';
+import { getServerSession, Session } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { getInfoSession } from '@/lib/next-auth';
+import { UserData } from '@/types/user';
+import { canEditGallery, getGallery } from '@/lib/galleries';
+import { NextPageContext } from 'next';
+import ViewPage from './viewpage';
+
+interface ViewPageProps {
+	params: {
+		galleryId: string;
+	};
+}
+
+async function fetchGallery(session: Session, galleryId: string) {
+	try {
+		const gallery = await getGallery(galleryId, session.user.email ?? '');
+		return gallery;
+	} catch (error) {
+		console.error('Failed to fetch gallery:', error);
+		throw error;
+	}
+}
+
+export default async function GalleryEditPage({ params }: ViewPageProps) {
+	const session = await getServerSession();
+
+	if (!session) {
+		redirect('/auth/signin');
+	}
+
+	if (!params.galleryId) {
+		redirect('/me');
+	}
+
+	const gallery = await fetchGallery(session, params.galleryId);
+
+	if (!gallery) {
+		redirect('/me');
+	}
+
+	const canEdit = await canEditGallery(params.galleryId, session.user.email as string);
+
+	return (
+		<div>
+			<ViewPage gallery={gallery} canEdit={canEdit} />
+		</div>
+	);
+}
---
+++ src/app/(root)/gallery/[galleryId]/viewpage.tsx
@@ -0,0 +1,42 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Gallery } from '@/types/gallery';
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Pencil } from 'lucide-react';
+import UploadForm from '@/components/upload-form';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Link from 'next/link';
+
+export default function ViewPage({ gallery, canEdit }: { gallery: Gallery; canEdit: boolean }) {
+	return (
+		<div className="md:space-y-6 space-y-3 p-5 pb-8 md:p-10 md:pb-16">
+			<div className="flex items-center justify-between">
+				<div className="space-y-0.5">
+					<h2 className="md:text-2xl text-xl font-bold tracking-tight">{gallery.galleryName}</h2>
+					<p className="text-muted-foreground md:text-base text-xs">{gallery.description}</p>
+				</div>
+				{canEdit}
+				{canEdit && (
+					<Button asChild>
+						<Link href={`/gallery/${gallery.publicId}/edit`}>
+							<Pencil className="mr-2 h-4 w-4" /> Edit gallery
+						</Link>
+					</Button>
+				)}
+			</div>
+
+			<div className="-ml-2 w-[calc(100%+1rem)]">
+				<ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 4, 900: 5 }}>
+					<Masonry>
+						{gallery?.images?.map((image, key) => (
+							<div key={'image' + key} className="m-2">
+								<img src={'/api/image/?imageUrl=' + image.imageUrl} alt={'image'} />
+							</div>
+						))}
+					</Masonry>
+				</ResponsiveMasonry>
+			</div>
+		</div>
+	);
+}
