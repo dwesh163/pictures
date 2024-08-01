a26ea3473f708b21457c25efa51a569d0b5ee4cb
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-01 16:01:42 +0200

    refactor: Add user profile and account data types

---
+++ src/types/user.ts
@@ -0,0 +1,22 @@
+// types.ts
+
+export type UserProfileData = {
+	username: string | null | undefined;
+	email: string | null | undefined;
+	bio: string | null | undefined;
+};
+
+export type UserAccountData = {
+	name: string | null | undefined;
+	birthday: string | null | undefined;
+	nameDisplay: boolean | null | undefined;
+};
+
+export type UserData = {
+	username?: string | null | undefined;
+	email?: string | null | undefined;
+	bio?: string | null | undefined;
+	name?: string | null | undefined;
+	birthday?: string | null | undefined;
+	nameDisplay?: boolean | null | undefined;
+};
