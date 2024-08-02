1d98a175508aed3e0770d9564712adb0bfb9719c
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-02 13:16:21 +0200

    chore: Add accreditationId field to UserData type

--- src/types/user.ts
+++ src/types/user.ts
@@ -19,6 +19,7 @@ export type UserData = {
 	name?: string | null | undefined;
 	birthday?: string | null | undefined;
 	nameDisplay?: boolean | null | undefined;
+	accreditationId?: number | null | undefined;
 };
 
 export type AccredUser = {
@@ -26,7 +27,7 @@ export type AccredUser = {
 	name: string;
 	email: string;
 	image: string;
-	accreditationId: string;
+	accreditationId: number;
 };
 
 export type UsersData = {
