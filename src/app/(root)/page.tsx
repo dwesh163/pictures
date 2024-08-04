0a4932cb3a1e1691aa1bcd994bbb284f8ce68598
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-04 18:26:21 +0200

    fix: Remove unused code in Page component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,4 @@ import { useSession } from 'next-auth/react';
 
 export default function Page() {
 	const { data: session, status } = useSession();
-
-	return <h1 className="text-white">{JSON.stringify(session)}</h1>;
 }
