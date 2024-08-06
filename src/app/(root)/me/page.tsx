871f22ed8a8b1d6affb17abff7229265ef42136a
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-06 21:22:37 +0200

    feat: Update import path for GalleriesList component in me/page.tsx

--- src/app/(root)/me/page.tsx
+++ src/app/(root)/me/page.tsx
@@ -3,16 +3,7 @@ import { redirect } from 'next/navigation';
 import { getInfoSession } from '@/lib/users';
 import { UserData } from '@/types/user';
 import { getGalleries } from '@/lib/galleries';
-import { Card, CardContent } from '@/components/ui/card';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { CameraOff, Eye, Lock, Plus, UserRound } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { GalleriesList } from './galleries-list';
+import { GalleriesList } from '@/components/galleries-list';
 import { CheckIfUserIsVerified } from '@/lib/admin';
 
 interface Gallery {
