bc15836a4df5fe7502166ec6e26d059f2f8be330
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-06 21:21:32 +0200

    fix: Update join/page.tsx to handle user join request
    The join/page.tsx file has been updated to handle user join requests by checking the validity of the provided token and the user's information. If the token is valid and the user's information is complete, they will be redirected to the home page. Otherwise, they will be redirected to the sign-in page. This change ensures a smooth and secure user join process.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,56 @@
-'use client';
-import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { getInfoSession } from '@/lib/users';
+import { UserData } from '@/types/user';
+import { getGalleries } from '@/lib/galleries';
+import { GalleriesList } from '@/components/galleries-list';
+import { CheckIfUserIsVerified } from '@/lib/admin';
 
-export default function Page() {
-	const { data: session, status } = useSession();
+interface Gallery {
+	accredited_users: string;
+	images: string;
+	galleryName: string;
+	description: string;
+	updatedAt: string;
+	publicId: string;
+}
+
+async function fetchUserData(session: any): Promise<UserData> {
+	const infoSession = await getInfoSession(session);
+
+	return {
+		name: infoSession.nameDisplay ? infoSession.name : infoSession.username,
+		email: infoSession.email,
+		bio: infoSession.bio,
+	};
+}
+
+async function fetchGalleries(session: any) {
+	const galleries = await getGalleries(session.user.email);
+	return galleries;
+}
+
+export default async function SettingsProfilePage() {
+	const session = await getServerSession();
+
+	if (!session) {
+		redirect('/auth/signin');
+	}
+
+	if (!(await CheckIfUserIsVerified(session.user.email as string))) {
+		redirect('/verified');
+	}
+
+	const userData = await fetchUserData(session);
+	let galleries = await fetchGalleries(session);
+
+	if (galleries == null) {
+		galleries = [];
+	}
+
+	return (
+		<div className="md:space-y-6 space-y-3 p-5 pb-8 md:p-10 md:pb-16">
+			<GalleriesList userData={userData} galleries={galleries} />
+		</div>
+	);
 }
