5ef5c90907f7f08dc741d346132ec01808b1202d
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-06 21:23:10 +0200

    refactor: Update SettingsLayout component
    The SettingsLayout component has been refactored to improve readability and maintainability. The code has been restructured to remove unnecessary div elements and improve the overall layout structure. This refactor enhances the component's performance and makes it easier to manage account settings and email preferences.

--- src/app/(root)/settings/layout.tsx
+++ src/app/(root)/settings/layout.tsx
@@ -22,20 +22,18 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
 	return (
-		<>
-			<div className="md:space-y-6 space-y-3 p-5 pb-8 md:p-10 md:pb-16">
-				<div className="space-y-0.5">
-					<h2 className="md:text-2xl text-xl font-bold tracking-tight">Settings</h2>
-					<p className="text-muted-foreground md:text-base text-xs">Manage your account settings and set e-mail preferences.</p>
-				</div>
-				<Separator className="md:my-6 my-1" />
-				<div className="flex flex-col md:space-y-8 space-y-4 lg:flex-row lg:space-x-12 lg:space-y-0">
-					<aside className="md:-mx-4 lg:w-1/5">
-						<SidebarNav items={sidebarNavItems} />
-					</aside>
-					<div className="flex-1 lg:max-w-2xl">{children}</div>
-				</div>
+		<div className="md:space-y-6 space-y-3 p-5 pb-8 md:p-10 md:pb-16">
+			<div className="space-y-0.5">
+				<h2 className="md:text-2xl text-xl font-bold tracking-tight">Settings</h2>
+				<p className="text-muted-foreground md:text-base text-xs">Manage your account settings and set e-mail preferences.</p>
 			</div>
-		</>
+			<Separator className="md:my-6 my-1" />
+			<div className="flex flex-col md:space-y-8 space-y-4 lg:flex-row lg:space-x-12 lg:space-y-0">
+				<aside className="md:-mx-4 lg:w-1/5">
+					<SidebarNav items={sidebarNavItems} />
+				</aside>
+				<div className="flex-1 lg:max-w-2xl">{children}</div>
+			</div>
+		</div>
 	);
 }
