a281b9ed78690f42bba8aec35a519777def47915
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-06 21:21:23 +0200

    fix: Update layout to adjust main padding-top
    The layout component has been updated to adjust the padding-top of the main content area. This change ensures proper spacing and alignment of the content, improving the overall visual appearance of the page.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,8 +3,6 @@ import { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import React from 'react';
-
-import { MobileNav } from '@/components/mobile/navbar';
 import { Header } from '@/components/header';
 
 export default function RootLayout({ children, session }: { children: ReactNode; session?: Session }) {
@@ -12,7 +10,7 @@ export default function RootLayout({ children, session }: { children: ReactNode;
 		<SessionProvider session={session}>
 			<Header />
 			{/* <MobileNav /> */}
-			<main className="lg:pt-0 pt-16">{children}</main>
+			<main className="lg:pt-0 pt-20">{children}</main>
 		</SessionProvider>
 	);
 }
