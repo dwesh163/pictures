ae8fb9beddbcd71de759215ae135edd6626cabde
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-07 21:20:14 +0200

    refactor: Add tags to Gallery type and create Image and Tags types

--- src/types/gallery.ts
+++ src/types/gallery.ts
@@ -13,4 +13,16 @@ export type Gallery = {
 	images: { userId: number; imageId: number; imageUrl: string }[] | null;
 	coverImage: any;
 	accredited_users: { name: string; email: string; image: string; accreditationId: number }[] | null;
+	tags: { id: number; name: string }[] | null;
+};
+
+export type Image = {
+	imageId: number;
+	userId: number;
+	imageUrl: string;
+};
+
+export type Tags = {
+	id: number;
+	name: string;
 };
