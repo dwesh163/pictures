261f772f1679b73fc263cf7d907920a752556469
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-07 19:55:41 +0200

    refactor: Update page component to include user-specific galleries

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,29 +1,58 @@
-import { getPublicGalleries } from '@/lib/galleries';
-import { MainGalleriesList } from '@/components/main-galleries-list';
-import { PublicGallery } from '@/types/gallery';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { getInfoSession } from '@/lib/users';
+import { UserData } from '@/types/user';
+import { getGalleries } from '@/lib/galleries';
+import { GalleriesList } from '@/components/galleries-list';
+import { CheckIfUserIsVerified } from '@/lib/admin';
 
-async function fetchPublicGalleries(): Promise<PublicGallery[]> {
-	try {
-		const publicGalleries = await getPublicGalleries();
-		console.log(publicGalleries);
-
-		throw new Error('Test error' + publicGalleries);
-
-		return publicGalleries || [];
-	} catch (error) {
-		console.error('Failed to fetch public galleries:', error);
-		return [];
-	}
+interface Gallery {
+	accredited_users: string;
+	images: string;
+	galleryName: string;
+	description: string;
+	updatedAt: string;
+	publicId: string;
 }
 
-export default async function MainPage() {
-	const publicGalleries = await fetchPublicGalleries();
+async function fetchUserData(session: any): Promise<UserData> {
+	const infoSession = await getInfoSession(session);
 
-	console.log(publicGalleries);
+	return {
+		name: infoSession.nameDisplay ? infoSession.name : infoSession.username,
+		email: infoSession.email,
+		bio: infoSession.bio,
+	};
+}
+
+async function fetchGalleries(session: any) {
+	const galleries = await getGalleries(session.user.email);
+	return galleries;
+}
+
+export default async function SettingsProfilePage() {
+	const session = await getServerSession();
+
+	if (!session) {
+		redirect('/auth/signin');
+	}
+
+	if (!(await CheckIfUserIsVerified(session.user.email as string))) {
+		redirect('/verified');
+	}
+
+	const userData = await fetchUserData(session);
+	let galleries = await fetchGalleries(session);
+
+	if (galleries == null) {
+		galleries = [];
+	}
+
+	console.log(galleries);
 
 	return (
 		<div className="md:space-y-6 space-y-3 p-5 pb-8 md:p-10 md:pb-16">
-			<MainGalleriesList galleries={publicGalleries} />
+			<GalleriesList userData={userData} galleries={galleries} />
 		</div>
 	);
 }
